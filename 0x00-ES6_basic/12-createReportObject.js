a455071be3356410495fcb1cd65d2b798ff58f2f
Dillys Annan <dillysannan@gmail.com>
2024-02-05 15:31:58 -0800

    tasks

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList){
+	return {
+	allEmployees: {...employeesList},
+	getNumberOfDepartments(allEmp) {
+	return Object.keys(allEmp).length;
+}
+
+}
+}
